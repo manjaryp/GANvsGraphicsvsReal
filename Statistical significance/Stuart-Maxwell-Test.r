b02cc830a3ea76f23b2e71f1359417d098850448
Manjary P Gangan <33519702+manjaryp@users.noreply.github.com>
2021-10-20 21:06:43 +0530

    Update Stuart-Maxwell-Test.r

--- Statistical significance/Stuart-Maxwell-Test.r
+++ Statistical significance/Stuart-Maxwell-Test.r
@@ -13,7 +13,7 @@ method_b_ss_psycho <- as.table(matrix(c(49, 9, 16, 35, 63, 29, 40, 25, 64), nrow
 stuart.maxwell(method_a_ss_psycho)
 StuartMaxwellTest(method_b_ss_psycho)
 
-#MC-EffNet-2 SCEffnet RGB
+#MC-EffNet-2 with SC-EffNet_RGB
 method_a_ss_scEffnet <- matrix(c(694, 79, 116, 57, 532, 144, 83, 138, 557), 3,3)
 method_b_ss_scEffnet <- as.table(matrix(c(694, 79, 116, 57, 532, 144, 83, 138, 557), nrow=3))
 stuart.maxwell(method_a_ss_scEffnet)
