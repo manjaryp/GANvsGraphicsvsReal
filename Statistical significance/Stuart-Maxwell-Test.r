889fd5749e4861749f8bfd11e7b9ca93519674fd
Manjary P Gangan <33519702+manjaryp@users.noreply.github.com>
2021-10-20 21:05:02 +0530

    Update Stuart-Maxwell-Test.r

--- Statistical significance/Stuart-Maxwell-Test.r
+++ Statistical significance/Stuart-Maxwell-Test.r
@@ -1,7 +1,7 @@
 library('nonpar')
 library('DescTools')
 
-#Our method with best baseline 
+#MC-EffNet-2 with best baseline 
 method_a_ss_baseline <- matrix(c(670, 101, 111, 55, 503, 123, 109, 145, 583), 3,3)
 method_b_ss_baseline <- as.table(matrix(c(670, 101, 111, 55, 503, 123, 109, 145, 583), nrow=3))
 stuart.maxwell(method_a_ss_baseline)
@@ -13,7 +13,7 @@ method_b_ss_psycho <- as.table(matrix(c(49, 9, 16, 35, 63, 29, 40, 25, 64), nrow
 stuart.maxwell(method_a_ss_psycho)
 StuartMaxwellTest(method_b_ss_psycho)
 
-#Our method SCEffnet RGB
+#MC-EffNet-2 SCEffnet RGB
 method_a_ss_scEffnet <- matrix(c(694, 79, 116, 57, 532, 144, 83, 138, 557), 3,3)
 method_b_ss_scEffnet <- as.table(matrix(c(694, 79, 116, 57, 532, 144, 83, 138, 557), nrow=3))
 stuart.maxwell(method_a_ss_scEffnet)
